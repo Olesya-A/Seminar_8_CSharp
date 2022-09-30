26d30d95c65243efa7f5b41f9c197619aa11c672
Olesya <lesusha1@yandex.ru>
2022-10-01 01:50:34 +0300

    add Task_55

---
+++ Task_55/Program.cs
@@ -0,0 +1,64 @@
+﻿// 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int row = EnterInt("Enter row: ");
+int col = EnterInt("Enter column: ");
+int[,] numbers = new int[row, col];
+
+Fill2DArray(numbers);
+Print2DArray(numbers);
+
+Console.WriteLine();
+
+Changed2DArray(numbers);
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Fill2DArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void Print2DArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write($"{numbers[i, j],2} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool IsPossible(int[,] numbers)
+{
+    return numbers.GetLength(0) == numbers.GetLength(1) || numbers.GetLength(0) == numbers.GetLength(1);
+}
+
+
+void Changed2DArray(int[,] numbers)
+{
+    if (IsPossible(numbers))
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.Write($"{numbers[j, i],2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+    else Console.WriteLine("Заменить строки на столбцы невозможно.");
+}
